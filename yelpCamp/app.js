d61c636db8050681d47b0bac47971aa66d388cd1
Kittur Sameer <42382545+vacool6@users.noreply.github.com>
2022-10-06 17:53:11 +0530

    Update mongoDB connection

--- yelpCamp/app.js
+++ yelpCamp/app.js
@@ -12,14 +12,10 @@ const Campground = require('./models/campground');
 const Review = require('./models/review');
 
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, "connection error:"));
-db.once('open', () => console.log("Database Connected :)"));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then((res) => console.log("Connection Successful to MongoDB"))
+  .catch((err) => console.log("Connection error"));
 
 const app = express();
 
